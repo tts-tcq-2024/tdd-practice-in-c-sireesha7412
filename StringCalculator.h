71067b884c12d5fe369ed98496747103583e19ae
sireesha7412 <saisireesha77777@gmail.com>
2024-07-09 11:59:21 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,8 +1,8 @@
 
 
-int add(const char*input)
+int add(const char* input)
 {
-if (str == NULL || str[0] == '\0') {
+if (input == NULL || input[0] == '\0') {
         return 1; // The string is empty
     }
  return -1;
