b4ab9188e33013517bc1d4062320a9edb58a5fb4
sireesha7412 <saisireesha77777@gmail.com>
2024-07-09 12:13:43 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,9 +1,11 @@
 
-
-int add(const char* input)
-{
+boolean isEmptyString(const char* input){
 if (input == NULL || input[0] == '\0') {
         return 0; // The string is empty
     }
+}
+int add(const char* input){
+        if(isEmptyString(input){ return 0;}
+        if given input contains single number which is < 1000 and unsigned with no delimiters then return same number 
  return -1;
 }
