1a98f83326e7fed612351486b710a13108c49dfd
sireesha7412 <saisireesha77777@gmail.com>
2024-07-15 22:26:40 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,76 +1,71 @@
 #include "StringCalculator.h"
 #include <sstream>
-#include <regex>
-#include <numeric>
-#include <iostream>
+#include <stdexcept>
 #include <algorithm>
-
-std::vector<std::string> StringCalculator::split(const std::string& str, const std::string& delimiter) {
-    std::vector<std::string> tokens;
-    std::string::size_type lastPos = 0, pos = str.find(delimiter, lastPos);
-
-    while (std::string::npos != pos) {
-        tokens.push_back(str.substr(lastPos, pos - lastPos));
-        lastPos = pos + delimiter.length();
-        pos = str.find(delimiter, lastPos);
-    }
-
-    tokens.push_back(str.substr(lastPos));
-    return tokens;
-}
-
-std::vector<int> StringCalculator::toInts(const std::vector<std::string>& tokens) {
-    std::vector<int> numbers;
-    for (const std::string& token : tokens) {
-        numbers.push_back(std::stoi(token));
-    }
-    return numbers;
-}
 
 int StringCalculator::add(const std::string& numbers) {
     if (numbers.empty()) {
         return 0;
     }
 
-    std::string delimiters = ",\n";
-    std::string numString = numbers;
+    std::string delimiter = ",|\n";
+    std::string numbersStr = numbers;
 
+    // Check for custom delimiter
     if (numbers.substr(0, 2) == "//") {
         size_t delimiterEndPos = numbers.find("\n");
-        std::string customDelimiter = numbers.substr(2, delimiterEndPos - 2);
-        
-        if (customDelimiter.front() == '[' && customDelimiter.back() == ']') {
-            customDelimiter = customDelimiter.substr(1, customDelimiter.length() - 2);
-        }
-        
-        delimiters += customDelimiter;
-        numString = numbers.substr(delimiterEndPos + 1);
+        delimiter = numbers.substr(2, delimiterEndPos - 2);
+        numbersStr = numbers.substr(delimiterEndPos + 1);
     }
 
-    std::regex re("[" + delimiters + "]");
-    std::sregex_token_iterator it(numString.begin(), numString.end(), re, -1);
-    std::sregex_token_iterator reg_end;
+    std::vector<int> nums = splitAndConvert(numbersStr, delimiter);
 
-    std::vector<std::string> tokens(it, reg_end);
-    std::vector<int> ints = toInts(tokens);
+    checkForNegatives(nums);
 
-    std::vector<int> negatives;
     int sum = 0;
-    for (int number : ints) {
-        if (number < 0) {
-            negatives.push_back(number);
-        } else if (number <= 1000) {
-            sum += number;
+    for (int num : nums) {
+        if (num <= 1000) {
+            sum += num;
         }
-    }
-
-    if (!negatives.empty()) {
-        std::string error = "negatives not allowed: ";
-        for (int n : negatives) {
-            error += std::to_string(n) + " ";
-        }
-        throw std::invalid_argument(error);
     }
 
     return sum;
+}
+
+std::vector<int> StringCalculator::splitAndConvert(const std::string& numbers, const std::string& delimiter) {
+    std::vector<int> result;
+    std::string num;
+    std::string::size_type lastPos = 0, pos = 0;
+
+    while ((pos = numbers.find_first_of(delimiter, lastPos)) != std::string::npos) {
+        if (pos > lastPos) {
+            result.push_back(std::stoi(numbers.substr(lastPos, pos - lastPos)));
+        }
+        lastPos = pos + 1;
+    }
+    if (lastPos < numbers.size()) {
+        result.push_back(std::stoi(numbers.substr(lastPos)));
+    }
+
+    return result;
+}
+
+void StringCalculator::checkForNegatives(const std::vector<int>& numbers) {
+    std::vector<int> negatives;
+    for (int num : numbers) {
+        if (num < 0) {
+            negatives.push_back(num);
+        }
+    }
+    if (!negatives.empty()) {
+        std::ostringstream oss;
+        oss << "negatives not allowed: ";
+        for (size_t i = 0; i < negatives.size(); ++i) {
+            oss << negatives[i];
+            if (i < negatives.size() - 1) {
+                oss << ", ";
+            }
+        }
+        throw std::runtime_error(oss.str());
+    }
 }
